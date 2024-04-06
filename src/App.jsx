f54a6f603c0be762e09f6fa4fb0b4bf7df5d3c45
fatemeh <fatemeh211100@gmail.com>
2024-04-06 23:44:50 +0330

    commit last 18

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,23 @@ import "./App.css";
 import CharacterDetails from "./components/CharacterDetails";
 import CharacterList from "./components/CharacterList";
 import Navbar, { SearchResult } from "./components/Navbar";
-import { allCharacters } from "../data/data";
 import { useState } from "react";
 
 function App() {
-  const [charecters, setCharecters] = useState(allCharacters);
+  const [charecters, setCharecters] = useState([]);
+  // not to fetch in this way =>
+  // fetch("https://rickandmortyapi.com/api/character")
+  //   .then((res) => res.json())
+  //   .then((data) => setCharecters(data.results)); // don't do in  renderlogic
+  // fetch API , timer , access to DOM
+  // effect : 1: event handler function , 2: useeffect
   return (
     <div className="app">
       <Navbar>
-        <SearchResult numOfResult={allCharacters.length} />
+        <SearchResult numOfResult={charecters.length} />
       </Navbar>
       <Main>
-        <CharacterList allCharacters={allCharacters} />
+        <CharacterList allCharacters={charecters} />
         <CharacterDetails />
       </Main>
     </div>
